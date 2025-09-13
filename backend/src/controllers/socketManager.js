dbef263b0402b2a150a61123e6f2742b0dc15b7f
Prateek Singhal <prateeksinghal682@gmail.com>
2025-09-13 14:42:36 +0530

    some code is added

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,26 +1,27 @@
 import { Server } from "socket.io"
 
 
-let connections = {}
-let messages = {}
-let timeOnline = {}
+let connections = {}  
+let messages = {} 
+let timeOnline = {} 
 
-const io = new Server(Server,{
+const io = new Server(Server,{ 
   cors: {
-    origin: [
-      "http://localhost:3000", // local frontend
-      "https://videochatapplicationfrontend.onrender.com" // Render frontend
+    origin: [ 
+      "http://localhost:3000", // local frontend 
+      "https://videochatapplicationfrontend.onrender.com" // Render frontend 
     ],
-    methods: ["GET", "POST"],
-    credentials: true
+    methods: ["GET", "POST"], 
+    credentials: true 
   }}
 );
 
-
+//new client came in
     io.on("connection", (socket) => {
 
         console.log("SOMETHING CONNECTED")
 
+        //new client joining the existing room or a new room
         socket.on("join-call", (path) => {
 
             if (connections[path] === undefined) {
@@ -30,15 +31,16 @@ const io = new Server(Server,{
 
             timeOnline[socket.id] = new Date();
 
-            // connections[path].forEach(elem => {
-            //     io.to(elem)
-            // })
 
+        //sending notifications to all clients 
             for (let a = 0; a < connections[path].length; a++) {
                 io.to(connections[path][a]).emit("user-joined", socket.id, connections[path])
             }
 
+
             if (messages[path] !== undefined) {
+
+                //loop through previous messages
                 for (let a = 0; a < messages[path].length; ++a) {
                     io.to(socket.id).emit("chat-message", messages[path][a]['data'],
                         messages[path][a]['sender'], messages[path][a]['socket-id-sender'])
@@ -49,7 +51,13 @@ const io = new Server(Server,{
 
         socket.on("signal", (toId, message) => {
             io.to(toId).emit("signal", socket.id, message);
-        })
+        })                                                                                                                      
+
+// reduce is looping over all rooms to find which room contains this socket.
+// It remembers the result in the accumulator [room, isFound].
+// Once found, it stops updating and keeps the correct room.
+// After the loop, you know which room the client belongs to.
+
 
         socket.on("chat-message", (data, sender) => {
 
