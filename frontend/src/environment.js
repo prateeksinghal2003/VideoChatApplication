c29bff737800fca3de685b2106b5059b85534c4f
Prateek Singhal <prateeksinghal682@gmail.com>
2025-09-29 20:13:50 +0530

    some code is added

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    " https://videochatapplicationbackendpart2.onrender.com" :
+    "https://videochatapplicationbackendpart2.onrender.com" :
 
     "http://localhost:8000"
 
